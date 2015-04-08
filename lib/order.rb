a7ea75eaa20b0c37b597a277c0f76bbccecc9b0e
Zeeshan Rasool <zrasool88@gmail.com>
2015-04-08 16:29:14 +0100

    order class an retreive line items. need to refactor its test. could be written clearly in one line

--- lib/order.rb
+++ lib/order.rb
@@ -3,11 +3,15 @@ class Order
 	attr_reader :items
 
 	def initialize
-		@items = {}
+		@items = []
 		@complete = false
 	end
 
 	def complete?
 		@complete
 	end
+
+  def add_line_item(line_item)
+    @items << line_item
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,16 +1,16 @@
 require 'order'
-# require 'dish'
-# require 'menu'
+require 'dish'
+require 'line_item'
 
 describe 'Order' do
 	let(:order){Order.new}
-  # let(:fish){Dish.new("fish", 10)}
-  # let(:chips){Dish.new("chips", 5)}
+  let(:fish){Dish.new("fish", 10)}
+  let(:line_item){LineItem.new(fish, 2)}
   # let(:coke){Dish.new("coke", 2)}
   # let(:menu){Menu.new}
 
-	it 'starts with an items hash' do
-		expect(order.items.class).to be Hash
+	it 'starts with an items array' do
+		expect(order.items.class).to be Array
 	end
 
   it 'starts with an empty items hash' do
@@ -20,4 +20,9 @@ describe 'Order' do
 	it 'is not complete upon initializing' do
 		expect(order.complete?).to be false
 	end
+
+  it "able to retrieve line items to and include it within itself" do
+    order.add_line_item(line_item)
+    expect(order.items.count).to eq 1
+  end
 end
